eb4cf77eb805f760e0eead9dbe584ee5c6007c09
Travis Cook <travis.cook5583@stu.mtec.edu>
2023-03-01 10:11:26 -0700

    Animated the REACT logo on the login page.

--- client/src/pages/loginPage.js
+++ client/src/pages/loginPage.js
@@ -8,23 +8,26 @@ export default function LoginPage() {
     return(
         <>
             <div id="crecent-box">
-                <div id="loginContainer">
-                    <h2>Login</h2>
-                    <form id="login-form">
-                        <span className="form-section">
-                            <label>Username: </label>
-                            <input name="username" id="username" placeholder="Type your username here."/>
-                        </span>
-                        <span className="form-section">
-                            <label>Password: </label>
-                            <input name="password" type="password" id="password" placeholder="Type your password here."/>
-                        </span>
-                        <span className="form-section">
-                            <button onClick={studentLogin()}>Login as a student</button>
-                            <button onClick={adminLogin()}>Login as an administrator</button>
-                        </span>
-                    </form>
-                    <Link to="/signUp" id="sign-up-button">New user?</Link>
+                <div id='image-helper'>
+                    <img src={require("../Images/logo192.png")} id="react-logo" alt='react-logo-green'/>
+                    <div id="loginContainer">
+                        <h2>Login</h2>
+                        <form id="login-form">
+                            <span className="form-section">
+                                <label>Username: </label>
+                                <input name="username" id="username" placeholder="Type your username here."/>
+                            </span>
+                            <span className="form-section">
+                                <label>Password: </label>
+                                <input name="password" type="password" id="password" placeholder="Type your password here."/>
+                            </span>
+                            <span className="form-section">
+                                <button onClick={studentLogin()}>Login as a student</button>
+                                <button onClick={adminLogin()}>Login as an administrator</button>
+                            </span>
+                        </form>
+                        <Link to="/signUp" id="sign-up-button">New user?</Link>
+                    </div>
                 </div>
             </div>
         </>
