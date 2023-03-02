881128726761be8de0645f85f09021fac561b303
Joseph Burton <joseph.burton6967@stu.mtec.edu>
2023-03-02 09:31:29 -0700

    added createUser to auth.controller.js; also created insertNewUser in db/index.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller.js');
 
+//authentication routes
+router.post('/login', authController.login);
+router.post('/studentLogin', authController.authUser);
+router.post('/userRegistration', authController.createUser);
 
 
 router.get('/userRegistration', (req, res) => {
