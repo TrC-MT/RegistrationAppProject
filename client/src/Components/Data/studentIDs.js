2157deb9ec2089a295ed2ebb9002b8737b4942e4
Travis Cook <travis.cook5583@stu.mtec.edu>
2023-03-06 09:50:48 -0700

    Student IDs filter properly.

--- client/src/Components/Data/studentIDs.js
+++ client/src/Components/Data/studentIDs.js
@@ -5,7 +5,8 @@ export default function StudentIDs({render}){
     for(let i = 0; i < render.student_IDs.length; i++){
         options[i] = <option value={render.student_IDs[i]}>{render.student_IDs[i]}</option>
     }
-    return options.filter(option => {
-        option.props.value.toString().includes(render.filter.toString())
-    })
+    const result = options.filter(option =>
+        option.props.value.toString().includes(`${render.filter}`)
+    )
+    return result
 }
