a3788736a818b1dea1c62e4917454c64af3bf692
Travis Cook <travis.cook5583@stu.mtec.edu>
2023-03-13 09:41:17 -0600

    CSS animations should be used more often in my opinion.

--- client/src/pages/errorPage.js
+++ client/src/pages/errorPage.js
@@ -9,7 +9,9 @@ export default function ErrorPage(){
             <h1 id="error-heading">ERROR: Page does not exist.</h1>
             <Link to="/" id="error-login-link">LOGIN</Link>
             <div id='error-image-box'>
-                <img src={require("../Images/logo192.png")} alt='react-logo-green' id='error-react-logo'/>
+                <a href='https://www.w3schools.com/css/css3_animations.asp' target='_blank'>
+                    <img src={require("../Images/logo192.png")} alt='react-logo-green' id='error-react-logo'/>
+                </a>
             </div>
         </>
     )
