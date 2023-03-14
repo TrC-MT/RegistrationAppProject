9ce512ffd68cbc6309aa02be5a3e7e39c7e11d0b
Travis Cook <travis.cook5583@stu.mtec.edu>
2023-03-14 08:32:26 -0600

    The loading bar is there.

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,6 @@ export default App;
 
 
 function checkToken(){
-  console.log('checkToken was run')
   // fetch('/authenticated', {
   //   method: 'GET',
   //   headers: {
@@ -47,7 +46,7 @@ function checkToken(){
   // })
 
   //These will be deleted. Comment out the return true depending on what pages you want.
-  return true;
+  // return true;
   return false;
 }
 
