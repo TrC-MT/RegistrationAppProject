795f8ae9540b1958eb9626ab65b3e4be3daae406
Travis Cook <travis.cook5583@stu.mtec.edu>
2023-03-21 09:54:24 -0600

    Admin update courses now functions properly.

--- client/src/Components/Data/adminCoursesTableTags.js
+++ client/src/Components/Data/adminCoursesTableTags.js
@@ -8,15 +8,6 @@ export default function AdminCoursesTableTags({render}){
     let filter = render.filter;
     let setNum = render.sn;
 
-    const [nName, setNName] = useState('');
-    const [nIdent, setNIdent] = useState();
-    const [nDesc, setNDesc] = useState('');
-    const [nTuit, setNTuit] = useState();
-    const [nCH, setNCH] = useState();
-    const [nP, setNP] = useState();
-    const [nCr, setNCr] = useState();
-    const [nMC, setNMC] = useState();
-
     let matchCourses = [];
     let j = -1;
     for(let i = 0; i < courses.length; i++){
@@ -25,31 +16,31 @@ export default function AdminCoursesTableTags({render}){
             matchCourses[j] = 
                 <tr className="table-course">
                     <td className="course-name">
-                        <input defaultValue={courses[i].name} onChange={(e) => setNName(e.target.value)}/>
+                        <input defaultValue={courses[i].name} onChange={(e) => courses[i].name = e.target.value}/>
                     </td>
                     <td className="course-id">
-                        <input defaultValue={courses[i].id} onChange={(e) => setNIdent(e.target.value)}/>
+                        <input defaultValue={courses[i].id} onChange={(e) => courses[i].id = e.target.value}/>
                     </td>
                     <td className="course-desc">
-                        <textarea defaultValue={courses[i].description} onChange={(e) => setNDesc(e.target.value)}/>
+                        <textarea defaultValue={courses[i].description} onChange={(e) => courses[i].description = e.target.value}/>
                     </td>
                     <td className="course-tuition">
-                        <input defaultValue={courses[i].tuition} onChange={(e) => setNTuit(e.target.value)}/>
+                        <input defaultValue={courses[i].tuition} onChange={(e) => courses[i].tuition = Number(e.target.value)}/>
                     </td>
                     <td className="course-hours">
-                        <input defaultValue={courses[i].creditHours} onChange={(e) => setNCH(e.target.value)}/>
+                        <input defaultValue={courses[i].creditHours} onChange={(e) => courses[i].creditHours = Number(e.target.value)}/>
                     </td>
                     <td className="course-period">
-                        <input defaultValue={courses[i].period} onChange={(e) => setNP(e.target.value)}/>
+                        <input defaultValue={courses[i].period} onChange={(e) => courses[i].period = e.target.value}/>
                     </td>
                     <td className="course-room">
-                        <input defaultValue={courses[i].room} onChange={(e) => setNCr(e.target.value)}/>
+                        <input defaultValue={courses[i].room} onChange={(e) => courses[i].room = e.target.value}/>
                     </td>
                     <td className="course-seats">
-                        <input defaultValue={courses[i].seats} onChange={(e) => setNMC(e.target.value)}/>
+                        <input defaultValue={courses[i].seats} onChange={(e) => courses[i].seats = Number(e.target.value)}/>
                     </td>
                     <td className="select-box">
-                       <button className={`course-select-button`} onClick={updateCourse}>UPDATE</button>
+                       <button className={`course-select-button`} onClick={(e) => updateCourse(i)}>UPDATE</button>
                     </td>
                 </tr>
         }
@@ -85,38 +76,45 @@ export default function AdminCoursesTableTags({render}){
         }
     }
 
-    function updateCourse(){
+    function updateCourse(i){
+        console.log(courses[i])
         if(
-            nName != ''
-            && nIdent != undefined
-            && nDesc != ''
-            && nTuit != undefined
-            && nCH != undefined
-            && nP != undefined
-            && nCr != undefined
-            && nMC != undefined
+            (courses[i].name != '' && courses[i].name != undefined && courses[i].name != null)
+            &&
+            (courses[i].id != '' && courses[i].id != undefined && courses[i].id != null)
+            &&
+            (courses[i].description != '' && courses[i].description != undefined && courses[i].description != null)
+            &&
+            (courses[i].tuition != '' && courses[i].tuition != undefined && courses[i].tuition != null)
+            &&
+            (courses[i].creditHours != '' && courses[i].creditHours != undefined && courses[i].creditHours != null)
+            &&
+            (courses[i].period != '' && courses[i].period != undefined && courses[i].period != null)
+            &&
+            (courses[i].room != '' && courses[i].room != undefined && courses[i].room != null)
+            &&
+            (courses[i].seats != '' && courses[i].seats != undefined && courses[i].seats != null)
         )
         {
-            
             fetch('/updateCourse', {
                 method: 'GET',
                 headers: {
                     "Content-Type": "application/json"
                 },
                 body: {
-                    name: nName,
-                    ID: nIdent,
-                    description: nDesc,
-                    tuition: nTuit,
-                    creditHours: nCH,
-                    period: nP,
-                    classroom: nCr,
-                    maxCapacity: nMC,
+                    name: courses[i].name,
+                    ID: courses[i].id,
+                    description: courses[i].description,
+                    tuition: courses[i].tuition,
+                    creditHours:  courses[i].creditHours,
+                    period: courses[i].period,
+                    classroom:  courses[i].room,
+                    maxCapacity:  courses[i].seats,
                 }
             })
             .then(res => res.json())
             .then(data => data)
-            
         }
+        
     }
 }
